bd47c2386fe9466496ecd56c027232c75fdf4074
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2025-04-03 11:43:11 +0900

    fix: lastName and firstName are reversed

--- src/mock/user.ts
+++ src/mock/user.ts
@@ -5,8 +5,8 @@ export const mockUsers: UserItem[] = [
   {
     id: 1,
     avatarUrl: "/assets/user/avatar.png",
-    firstName: "求職",
-    lastName: "引担",
+    lastName: "求職",
+    firstName: "引担",
     prefecture: "愛知県",
     university: "名古屋大学",
     faculty: "文学部",
@@ -22,8 +22,8 @@ export const mockUsers: UserItem[] = [
   {
     id: 2,
     avatarUrl: "/assets/user/avatar.png",
-    firstName: "学業",
-    lastName: "専念",
+    lastName: "学業",
+    firstName: "専念",
     prefecture: "山口県",
     university: "山口大学",
     faculty: "工学部",
