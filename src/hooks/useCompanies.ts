d1ce4d1c6bc42a73e07006690b2f23d12261b176
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2025-04-28 00:54:32 +0900

    feature: add pagenation for companies

--- src/hooks/useCompanies.ts
+++ src/hooks/useCompanies.ts
@@ -4,19 +4,25 @@ import { CompanyFilterFormData } from "@/schemas/company/filter";
 import { SEARCH_COMPANY } from "@/server/graphql/company/queries";
 import { CompanyItem } from "@/types/company";
 
-export function useCompanies(initialInput: CompanyFilterFormData) {
+export function useCompanies(
+  initialInput: CompanyFilterFormData,
+  page: number,
+  limit: number,
+) {
   const [input, setInput] = useState(initialInput);
   const [companies, setCompanies] = useState<CompanyItem[]>([]);
   const [totalCount, setTotalCount] = useState(0); // 検索結果数(全ページ)
+  const [totalPages, setTotalPages] = useState(0); // 一覧表のページ数
   const [loading, setLoading] = useState(true);
 
   // 求職者一覧情報を取得
   useQuery(SEARCH_COMPANY, {
-    variables: { input: { ...input, limit: 30 } },
+    variables: { input: { ...input, page, limit } },
     fetchPolicy: "no-cache",
     onCompleted(result) {
       setTotalCount(result.getCompanies.totalCount);
       setCompanies(result.getCompanies.items);
+      setTotalPages(result.getCompanies.totalPages);
       setLoading(false);
     },
     onError() {
@@ -34,6 +40,7 @@ export function useCompanies(initialInput: CompanyFilterFormData) {
   return {
     companies,
     totalCount,
+    totalPages,
     loading,
     refetchCompanies,
   };
