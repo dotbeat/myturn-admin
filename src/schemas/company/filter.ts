3ee279fbb37aaf5d6563b9c468c433a3d8aab6b7
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2025-04-28 15:23:07 +0900

    fix: prevent error when clear date inputs of company filter

--- src/schemas/company/filter.ts
+++ src/schemas/company/filter.ts
@@ -1,12 +1,13 @@
 import { z } from "zod";
+import { ruleDateNullable } from "../common/date";
 
 export const companyFilterFormSchema = z.object({
   name: z.string().trim(), // 企業名
   prefecture: z.string(), // 都道府県
-  registerDateStart: z.string().length(10).nullable(), // 登録日(開始)
-  registerDateEnd: z.string().length(10).nullable(), // 登録日(終了)
-  leaveDateStart: z.string().length(10).nullable(), // 退会日(開始)
-  leaveDateEnd: z.string().length(10).nullable(), // 退会日(終了)
+  registerDateStart: ruleDateNullable(), // 登録日(開始)
+  registerDateEnd: ruleDateNullable(), // 登録日(終了)
+  leaveDateStart: ruleDateNullable(), // 退会日(開始)
+  leaveDateEnd: ruleDateNullable(), // 退会日(終了)
   industry: z.string(), // 業界
   jobCountMin: z.coerce.number().min(0), // 掲載数(最小)
   jobCountMax: z.coerce.number().min(0), // 掲載数(最大)
