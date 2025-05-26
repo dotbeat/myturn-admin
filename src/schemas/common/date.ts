ca10933423ec9073be76f869d3ebebfc5899ef11
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2025-05-26 21:10:56 +0900

    fix: date should validate as date but provide as string

--- src/schemas/common/date.ts
+++ src/schemas/common/date.ts
@@ -1,8 +1,14 @@
 import { z } from "zod";
 
 export function ruleDateNullable(message?: string) {
-  return z.preprocess(
-    (value) => (value ? new Date(value as string) : null),
-    z.date({ message }).nullable(),
-  ); // 登録日(開始)
+  return z.custom<string | null>(
+    (value: string | null) => {
+      if (value == null) {
+        return true;
+      }
+      const valueAsDate = new Date(value);
+      return !Number.isNaN(valueAsDate.getTime());
+    },
+    { message },
+  );
 }
