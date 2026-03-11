850fd880f461be378bab16590c981287ea37f2a0
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2026-03-11 21:39:51 +0900

    feature: add period filters for applicants

--- src/schemas/common/date.ts
+++ src/schemas/common/date.ts
@@ -1,14 +1,17 @@
 import { z } from "zod";
 
 export function ruleDateNullable(message?: string) {
-  return z.custom<string | null>(
-    (value: string | null) => {
-      if (value == null) {
-        return true;
-      }
-      const valueAsDate = new Date(value);
-      return !Number.isNaN(valueAsDate.getTime());
-    },
-    { message },
+  return z.preprocess(
+    (value) => value || null,
+    z.custom<string | null>(
+      (value: string | null) => {
+        if (value == null) {
+          return true;
+        }
+        const valueAsDate = new Date(value);
+        return !Number.isNaN(valueAsDate.getTime());
+      },
+      { message },
+    ),
   );
 }
